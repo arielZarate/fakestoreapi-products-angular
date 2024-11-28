04b0c104b7a4848da702df0364807ae395d65434
arielZarate <arieltecnico@gmail.com>
2024-11-27 22:19:19 -0300

    navbar modificado con dropdowns menu y otras opciones

--- src/app/shared/components/layout/navbar/navbar.component.ts
+++ src/app/shared/components/layout/navbar/navbar.component.ts
@@ -15,13 +15,23 @@ export class NavbarComponent {
  //=======dropdown========================================== 
 
 isMenuOpen:boolean = false;
+//=======menu================================
+islogguedIn:boolean = true;  // si esta logueado hace una cosa y sino otra , debe venir del login
+//=======dropdown==========================================
 
+isDropdownOpen:boolean = false;
 
  // Alternar estado del menú
  toggleMenu() {
   this.isMenuOpen = !this.isMenuOpen;
 }
 
+
+
+
+toggleDropdown() {
+  this.isDropdownOpen =!this.isDropdownOpen;
+}
 // Listener para el tamaño de la pantalla
 @HostListener('window:resize', ['$event'])
 onResize(event: Event) {
@@ -30,7 +40,7 @@ onResize(event: Event) {
   // Detecta el tamaño de la pantalla y ajusta el estado del menú
   handleScreenResize() {
     const screenWidth = window.innerWidth;
-    if (screenWidth > 700) {
+    if (screenWidth > 640) {
       this.isMenuOpen = false;  // Cierra el menú en pantallas grandes
     } else if (this.isMenuOpen) {
       this.isMenuOpen = true;   // Si el menú está abierto, mantenlo abierto en pantallas pequeñas
