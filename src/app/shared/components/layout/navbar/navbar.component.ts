4f577a846c7ea9d477c2323712c72126356ffe4a
arielZarate <arieltecnico@gmail.com>
2024-11-28 00:18:21 -0300

    navbar modificado barra de navegacion con iconos

--- src/app/shared/components/layout/navbar/navbar.component.ts
+++ src/app/shared/components/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component,HostListener  } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { SwitcherButtonComponent } from '../switcher-button/switcher-button.component';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -6,44 +6,44 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [SwitcherButtonComponent,RouterLink,CommonModule],
+  imports: [SwitcherButtonComponent, RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
- // styleUrl: './navbar.component.css',
+  // styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  //=======dropdown==========================================
 
- //=======dropdown========================================== 
+  isMenuOpen: boolean = false;
+  //=======menu================================
+  islogguedIn: boolean = true; // si esta logueado hace una cosa y sino otra , debe venir del login
+  //=======dropdown==========================================
 
-isMenuOpen:boolean = false;
-//=======menu================================
-islogguedIn:boolean = true;  // si esta logueado hace una cosa y sino otra , debe venir del login
-//=======dropdown==========================================
+  isDropdownOpen: boolean = false;
 
-isDropdownOpen:boolean = false;
+  // Alternar estado del menú
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
- // Alternar estado del menú
- toggleMenu() {
-  this.isMenuOpen = !this.isMenuOpen;
-}
-
-
-
-
-toggleDropdown() {
-  this.isDropdownOpen =!this.isDropdownOpen;
-}
-// Listener para el tamaño de la pantalla
-@HostListener('window:resize', ['$event'])
-onResize(event: Event) {
-  this.handleScreenResize();
-}
+  //boton del dropDown
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+  // Listener para el tamaño de la pantalla
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.handleScreenResize();
+  }
   // Detecta el tamaño de la pantalla y ajusta el estado del menú
   handleScreenResize() {
     const screenWidth = window.innerWidth;
     if (screenWidth > 640) {
-      this.isMenuOpen = false;  // Cierra el menú en pantallas grandes
-    } else if (this.isMenuOpen) {
-      this.isMenuOpen = true;   // Si el menú está abierto, mantenlo abierto en pantallas pequeñas
+     // console.log('Screen', screenWidth);
+      this.isMenuOpen = false; // Cierra el menú en pantallas grandes
+    } else {
+     // console.log('menu open', this.isMenuOpen);
+
+      this.isMenuOpen = true; // Si el menú está abierto, mantenlo abierto en pantallas pequeñas
     }
   }
 
@@ -51,7 +51,6 @@ onResize(event: Event) {
   ngOnInit() {
     this.handleScreenResize();
   }
-
 
   // Puedes llamar a esta función con un botón o evento
 }
